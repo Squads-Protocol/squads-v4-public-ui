e3cb87f2eb4de6e53cf189d9736f62ee8978bd96
Valentin Madrid <valentin@incility.com>
2024-01-15 18:32:45 +0100

    more components

---
+++ components/VaultDisplayer.tsx
@@ -0,0 +1,32 @@
+import * as multisig from "@sqds/multisig"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { PublicKey } from "@solana/web3.js";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { VaultSelector } from "./VaultSelector";
+
+type VaultDisplayerProps = {
+    multisigPdaString: string,
+    vaultIndex: number
+  };
+
+  
+export function VaultDisplayer({multisigPdaString, vaultIndex}: VaultDisplayerProps) {
+    const vaultAddress = multisig.getVaultPda({
+        multisigPda: new PublicKey(multisigPdaString),
+        index: vaultIndex
+    })
+
+    return (
+        <Card className="w-fit my-3">
+            <CardHeader>
+                <CardTitle>Squads Vault</CardTitle>
+            </CardHeader>
+          <CardContent>
+            <p className="mb-1">Address: {vaultAddress[0].toBase58()}</p>
+            <br />
+            <VaultSelector />
+
+          </CardContent>
+      </Card>
+    )
+};
---
+++ components/VaultSelector.tsx
@@ -0,0 +1,80 @@
+"use client"
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useRouter } from "next/navigation";
+
+// Generate vault indices from 0 to 255
+const vaultIndices = Array.from({ length: 16 }, (_, index) => ({
+  value: index.toString(),
+  label: `Vault ${index}`,
+}));
+
+export function VaultSelector() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  const router = useRouter();
+
+  // useEffect hook to trigger an action when value changes
+  React.useEffect(() => {
+    document.cookie = `x-vault-index=${value}; path=/`;
+    router.refresh();
+  }, [value]);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value ? `Vault ${value}` : "Select Vault Index..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search Vault Index..." />
+          <CommandEmpty>No vault index found.</CommandEmpty>
+          <CommandGroup>
+            {vaultIndices.map((vaultIndex) => (
+              <CommandItem
+                key={vaultIndex.value}
+                value={vaultIndex.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? "" : currentValue);
+                  setOpen(false);
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    value === vaultIndex.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {vaultIndex.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
