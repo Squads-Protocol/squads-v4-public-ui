83b8ff2bab6fa7b6d12ac355e4d4bf9bf8e32cb2
Valentin Madrid <94240868+valentinmadrid@users.noreply.github.com>
2024-03-08 17:32:47 +0000

    add custom priority fees on execution

--- components/ExecuteButton.tsx
+++ components/ExecuteButton.tsx
@@ -1,11 +1,16 @@
 "use client";
-import { Connection, PublicKey, Transaction } from "@solana/web3.js";
+import { ComputeBudgetInstruction, ComputeBudgetProgram, Connection, PublicKey, Transaction } from "@solana/web3.js";
 import { Button } from "./ui/button";
 import * as multisig from "@sqds/multisig";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { Dialog, DialogDescription, DialogHeader } from "./ui/dialog";
+import { DialogTrigger } from "./ui/dialog";
+import { DialogContent, DialogTitle } from "./ui/dialog";
+import { useState } from "react";
+import { Input } from "./ui/input";
 
 type ExecuteButtonProps = {
   rpcUrl: string;
@@ -23,6 +28,8 @@ const ExecuteButton = ({
   const wallet = useWallet();
   const walletModal = useWalletModal();
   const router = useRouter();
+  const [priorityFeeLamports, setPriorityFeeLamports] = useState<number>(5000)
+  const [computeUnitBudget, setComputeUnitBudget] = useState<number>(200_000)
 
   const isTransactionReady = proposalStatus === "Approved";
   const connection = new Connection(rpcUrl, { commitment: "confirmed" });
@@ -38,15 +45,23 @@ const ExecuteButton = ({
       toast.error("Proposal has not reached threshold.");
       return;
     }
-    const executeTransaction =
-      await multisig.transactions.vaultTransactionExecute({
+
+    let executeTransaction = new Transaction();
+    const executeInstruction =
+      await multisig.instructions.vaultTransactionExecute({
         multisigPda: new PublicKey(multisigPda),
         connection,
         member: wallet.publicKey,
         transactionIndex: bigIntTransactionIndex,
-        blockhash: (await connection.getLatestBlockhash()).blockhash,
-        feePayer: wallet.publicKey,
       });
+
+      const priorityFeeInstruction = ComputeBudgetProgram.setComputeUnitPrice({microLamports: priorityFeeLamports});
+      const computeUnitInstruction = ComputeBudgetProgram.setComputeUnitLimit({units: computeUnitBudget});
+
+      executeTransaction.add(computeUnitInstruction, priorityFeeInstruction, executeInstruction.instruction)
+
+      executeTransaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+      executeTransaction.feePayer = wallet.publicKey;
 
     const signature = await wallet.sendTransaction(
       executeTransaction,
@@ -63,14 +78,35 @@ const ExecuteButton = ({
     router.refresh();
   };
   return (
-    <Button
-      disabled={!isTransactionReady}
-      onClick={executeTransaction}
-      className="mr-2"
-    >
-      Execute
-    </Button>
-  );
+    <Dialog>
+      <DialogTrigger disabled={!isTransactionReady}>
+        <Button
+          disabled={!isTransactionReady}
+          className="mr-2"
+        >Execute</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Execute Transaction</DialogTitle>
+          <DialogDescription>
+            Select custom priority fees and compute unit limits and execute transaction.
+          </DialogDescription>
+        </DialogHeader>
+        <h3>Priority Fee in lamports</h3>
+        <Input placeholder="Priority Fee" onChange={(e) => setPriorityFeeLamports(Number(e.target.value))} value={priorityFeeLamports} />
+
+        <h3>Compute Unit Budget</h3>
+        <Input placeholder="Priority Fee" onChange={(e) => setComputeUnitBudget(Number(e.target.value))} value={computeUnitBudget} />
+        <Button
+          disabled={!isTransactionReady}
+          onClick={executeTransaction}
+          className="mr-2"
+        >
+          Execute
+        </Button>
+      </DialogContent>
+    </Dialog>
+  )
 };
 
 export default ExecuteButton;
