2ffd7bdf9aa1f63a29ebcd0fab966659300b5e98
Joey Meere <100378695+joeymeere@users.noreply.github.com>
2024-10-15 15:05:54 -0400

    misc(ui): add sample memo transaction
    for testing purposes, and as a working example of how to encode a transaction

--- components/CreateTransactionButton.tsx
+++ components/CreateTransactionButton.tsx
@@ -7,10 +7,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import * as bs58 from "bs58";
 import { Button } from "./ui/button";
 import { useState } from "react";
+import * as multisig from "@sqds/multisig";
 import { useWallet } from "@solana/wallet-adapter-react";
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import {
+  Connection,
+  PublicKey,
+  TransactionInstruction,
+  TransactionMessage,
+  clusterApiUrl,
+} from "@solana/web3.js";
 import { Input } from "./ui/input";
 import { toast } from "sonner";
 import { simulateEncodedTransaction } from "@/lib/transaction/simulateEncodedTransaction";
@@ -38,6 +46,38 @@ const CreateTransaction = ({
     commitment: "confirmed",
   });
 
+  const getSampleMessage = async () => {
+    let memo = "Hello from Solana land!";
+    const vaultAddress = multisig.getVaultPda({
+      index: vaultIndex,
+      multisigPda: new PublicKey(multisigPda),
+      programId: programId ? new PublicKey(programId) : multisig.PROGRAM_ID,
+    })[0];
+    const dummyMessage = new TransactionMessage({
+      instructions: [
+        new TransactionInstruction({
+          keys: [
+            {
+              pubkey: wallet.publicKey as PublicKey,
+              isSigner: true,
+              isWritable: true,
+            },
+          ],
+          data: Buffer.from(memo, "utf-8"),
+          programId: new PublicKey(
+            "MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr"
+          ),
+        }),
+      ],
+      payerKey: vaultAddress,
+      recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
+    }).compileToV0Message();
+
+    const encoded = bs58.default.encode(dummyMessage.serialize());
+
+    setTx(encoded);
+  };
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>
@@ -54,6 +94,7 @@ const CreateTransaction = ({
         <Input
           placeholder="Paste base58 encoded transaction..."
           type="text"
+          defaultValue={tx}
           onChange={(e) => setTx(e.target.value)}
         />
         <div className="flex gap-2 items-center justify-end">
@@ -98,6 +139,12 @@ const CreateTransaction = ({
             Import
           </Button>
         </div>
+        <button
+          onClick={() => getSampleMessage()}
+          className="flex justify-end text-xs underline text-stone-400 hover:text-stone-200 cursor-pointer"
+        >
+          Click to use a sample memo for testing
+        </button>
       </DialogContent>
     </Dialog>
   );
